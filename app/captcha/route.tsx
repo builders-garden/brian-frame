2681cf9f4146dec268be638d2c037961bdf54dd9
lim0n3 <simonestaffa96@gmail.com>
2024-03-23 20:07:25 +0100

    font data

--- app/captcha/route.tsx
+++ app/captcha/route.tsx
@@ -1,22 +1,30 @@
 import { createFrames, Button } from "frames.js/next";
 import { generateCaptchaChallenge } from "../../lib/captcha";
+import { join } from "path";
+import * as fs from "fs";
 
 const frames = createFrames();
 const handleRequest = frames(async () => {
+  const fontPath = join(process.cwd(), "Changa-VariableFont.ttf");
+  let fontData = fs.readFileSync(fontPath);
+
   const { id, numA, numB } = await generateCaptchaChallenge();
   console.log("Generated captcha challenge", { id, numA, numB });
   return {
     postUrl: "/captcha/validate?id=" + id,
     image: (
-      <div
-        tw="text-blue-500 flex"
-        style={{
-          fontFamily: "Changa-VariableFont",
-        }}
-      >
+      <div tw="text-blue-500 flex">
         {numA} + {numB} = ?
       </div>
     ),
+    imageOptions: {
+      fonts: [
+        {
+          data: fontData,
+          name: "Changa",
+        },
+      ],
+    },
     textInput: "Enter the result",
     buttons: [
       <Button action="post" key="1" target={`/captcha/validate?id=${id}`}>
