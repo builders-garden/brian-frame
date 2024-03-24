059291a47cdf45249bf51db1f37b100e1454addb
francescocirulli <63744530+francescocirulli@users.noreply.github.com>
2024-03-24 10:11:22 +0100

    pinata analytics

--- lib/pinata-analytics.ts
+++ lib/pinata-analytics.ts
@@ -1,4 +1,9 @@
 import { PinataFDK } from "pinata-fdk";
-const fdk = new PinataFDK();
 
-fdk.sendAnalytics()
+const pinataGateway = process.env.PINATA_GATEWAY!;
+const pinataJWT = process.env.PINATA_JWT!;
+
+const fdk = new PinataFDK({
+    pinata_jwt: pinataJWT,
+    pinata_gateway: pinataGateway,}, 
+);
